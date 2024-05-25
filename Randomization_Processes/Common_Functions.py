8be8aca596e9cb27ca8275e4d1ef3d4af8ab7822
Daniel Rivas <drivas8891@gmail.com>
2024-05-24 20:08:00 -0700

    Refactor conversion functions for readability and efficiency.
    - Simplified the logic in possible_negative and fit_for_hex functions
    - Used list comprehension for get_address_endpoints

--- Randomization_Processes/Common_Functions.py
+++ Randomization_Processes/Common_Functions.py
@@ -49,26 +49,21 @@ def dump_json(json_file_dir, use_this_dict):
 
 def possible_negative(int_val):
     '''If value would be a negative hex value (greater than 0x8000), converts to negative. Otherwise returns initial value'''
-    if(int_val > 32768):
-        int_val = int_val - 65536
+    if(int_val > 0x8000):
+        int_val -= 0x10000
     return int_val
 
 def fit_for_hex(int_val):
     '''Prepares value for hex conversion'''
-    while(int_val < 0):
-        int_val = int_val + 65536
-    while(int_val > 65536):
-        int_val = int_val - 65536
+    int_val %= 0x10000
     return int_val
 
 def get_address_endpoints(file_bytes, addr):
     """Goes to address (found in Banjo's Backpack) and address 8 bytes after to find the start and end of a setup file"""
-    byte_list = []
-    for byte_num in range(16):
-        byte_val = str(hex(file_bytes[int(addr, 16) + byte_num])[2:])
-        if(len(str(byte_val)) < 2):
-            byte_val = "0" + byte_val
-        byte_list.append(byte_val)
-    address1 = int("0x" + byte_list[0] + byte_list[1] + byte_list[2] + byte_list[3], 16) + int("0x10CD0", 16)
-    address2 = int("0x" + byte_list[8] + byte_list[9] + byte_list[10] + byte_list[11], 16) + int("0x10CD0", 16)
-    return (address1, address2)
+    addr_int = int(addr, 16)
+    byte_list = [f"{file_bytes[addr_int + byte_num]:02x}" for byte_num in range(16)]
+    
+    address1 = int("".join(byte_list[:4]), 16) + 0x10CD0
+    address2 = int("".join(byte_list[8:12]), 16) + 0x10CD0
+    
+    return address1, address2
